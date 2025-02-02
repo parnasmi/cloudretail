b9c36369190dc5b8925d0e159b3a279452ec9663
IlhomMax <parnasmi@gmail.com>
2025-02-02 15:51:01 +0500

    feat:27-setup npm run all, setup tailwind prettier plugin

--- apps/ui/src/App.tsx
+++ apps/ui/src/App.tsx
@@ -1,5 +1,9 @@
 import React, { FC } from 'react';
 
 export const App: FC = () => {
-  return <div>App</div>;
+  return (
+    <div className="text-red border bg-indigo-300 p-1 text-2xl">
+      <span>CloudRetail</span>
+    </div>
+  );
 };
