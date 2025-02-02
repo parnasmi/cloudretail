1d64baab201de291ba7c2c42d1f874a78d0e04e7
IlhomMax <parnasmi@gmail.com>
2025-02-02 17:24:39 +0500

    feat: add routing and base components

---
+++ apps/ui/src/pages/Login/Login.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Button } from '../../shared/ui/Button/Button';
+
+export const Login: React.FC = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-50 px-4 py-12 sm:px-6 lg:px-8">
+      <div className="w-full max-w-md space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+        <form className="mt-8 space-y-6">
+          <div className="-space-y-px rounded-md shadow-sm">
+            <div>
+              <label htmlFor="email-address" className="sr-only">
+                Email address
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="relative block w-full rounded-t-md border-0 p-1.5 text-gray-900 ring-1 ring-gray-300 ring-inset placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-indigo-600 focus:ring-inset"
+                placeholder="Email address"
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="relative block w-full rounded-b-md border-0 p-1.5 text-gray-900 ring-1 ring-gray-300 ring-inset placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-indigo-600 focus:ring-inset"
+                placeholder="Password"
+              />
+            </div>
+          </div>
+          <div>
+            <Button color="primary" className="w-full">
+              Sign in
+            </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
